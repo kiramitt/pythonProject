40568e6ad8c67350aa59e6a19a051f8ea5009d97
Kira Mitt <73468778+kiramitt@users.noreply.github.com>
2024-03-24 22:21:58 +0300

    Update test_auth.py

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -7,6 +7,6 @@ def test_auth_positive():
     browser.get('https://www.saucedemo.com/v1/')
     browser.find_element("xpath", '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
-    # browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
     assert browser.current_url == 'https://www.saucedemo.com/v1/inventory.html', 'url не соответствует ожидаемому'
     browser.quit()
