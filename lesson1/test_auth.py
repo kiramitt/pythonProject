f41186ac505a887ed2c0798f3946181c6ce2860e
kiramitt <kiramitt@gmail.com>
2024-03-26 22:56:55 +0300

    work

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
+import time
 
-browser = webdriver.Chrome()
+
 def test_auth_positive():
+    browser = webdriver.Chrome()
     browser.get('https://www.saucedemo.com/v1/')
     browser.find_element("xpath", '//*[@id="user-name"]').send_keys('standard_user')
     browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
