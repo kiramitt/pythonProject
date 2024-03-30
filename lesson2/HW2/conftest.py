9376dd8d8722611d5b04682b932b0f7971194a5c
kiramitt <kiramitt@gmail.com>
2024-03-30 22:43:00 +0300

    work

---
+++ lesson2/HW2/conftest.py
@@ -0,0 +1,49 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from faker import Faker
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser(request):
+    print(f'\nStart test: {request.node.name}')
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    browser = webdriver.Chrome(options=chrome_options)
+    # browser = webdriver.Chrome()
+    browser.implicitly_wait(3)
+    yield browser
+    browser.quit()
+    print("\nEnd test")
+
+
+@pytest.fixture
+def fake_password():
+    fake = Faker()
+    return fake.password()
+
+
+@pytest.fixture
+def fake_username():
+    fake = Faker()
+    return fake.user_name()
+
+
+@pytest.fixture
+def fake_firstname():
+    fake = Faker()
+    return fake.first_name()
+
+
+@pytest.fixture
+def fake_lastname():
+    fake = Faker()
+    return fake.last_name()
+
+
+@pytest.fixture
+def fake_zipcode():
+    fake = Faker()
+    return fake.zipcode()
---
+++ lesson2/HW2/data.py
@@ -0,0 +1,28 @@
+# sleep_time = 0.075
+# URLS
+main_url = 'https://www.saucedemo.com/'
+catalog_url = f'{main_url}inventory.html'
+card_url = f'{main_url}inventory-item.html?id=4'
+complete_order_url = f'{main_url}checkout-complete.html'
+about_url = 'https://saucelabs.com/'
+
+# DATA
+# auth
+username_pos = "standard_user"
+password_pos = "secret_sauce"
+
+# texts
+error_auth_neg = "Epic sadface: Username and password do not match any user in this service"
+order_pos_text = "Thank you for your order!"
+
+# value
+# number_in_cart_value = "1"
+select_az = "az"
+select_za = "za"
+select_lohi = "lohi"
+select_hilo = "hilo"
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+register_form_url = "https://victoretc.github.io/webelements_information/"
+
